3545129b191ac86de8852eacd8e13e4ec2e39cb2
Abhishek Anand <76611051+abhianand0110@users.noreply.github.com>
2022-04-22 00:19:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ let packageDirectory = __dirname + '/package/';
 eval(fs.readFileSync(packageDirectory + 'c-lexer.min.js', 'utf8').toString());
 let cLexer = lexer;
 
-eval(fs.readFileSync(packageDirectory + 'sql-lexer.min.js', 'utf8').toString());
-let sqlLexer = lexer;
-
 module.exports = {
     cLexer: cLexer,
-    sqlLexer: sqlLexer,
 };
